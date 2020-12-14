3c708cdf29d5b01a378173b4598a6e7dbc4c0297
LENOVO <emyrafik.14@gmail.com>
2020-12-14 14:55:49 +0200

    greedy agent

---
+++ src/models/State.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class State implements Comparator<State>{
+
+	private List<Integer> playerTerritories;
+	//private List<Integer> opponentTerritories;
+	
+	private int cost, heuristic;
+	
+	
+	public List<Integer> getPlayerTerritories() {
+		return playerTerritories;
+	}
+
+
+	public void setPlayerTerritories(List<Integer> playerTerritories) {
+		this.playerTerritories = playerTerritories;
+	}
+
+
+//	public List<Integer> getOpponentTerritories() {
+//		return opponentTerritories;
+//	}
+//
+//
+//	public void setOpponentTerritories(List<Integer> opponentTerritories) {
+//		this.opponentTerritories = opponentTerritories;
+//	}
+
+
+	public int getCost() {
+		return cost;
+	}
+
+
+	public void calculateCost() {
+		
+	}
+
+
+	public int getHeuristic() {
+		return heuristic;
+	}
+
+
+	public void calculateHeuristic() {
+		
+	}
+
+
+	@Override
+	public int compare(State s1, State s2) {
+		// TODO Auto-generated method stub
+		return s2.getHeuristic() - s1.getHeuristic();
+	}
+
+}
--- src/models/agents/GreedyAgent.java
+++ src/models/agents/GreedyAgent.java
@@ -1,9 +1,18 @@
 package models.agents;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import models.Game;
 import models.Player;
+import models.State;
 
 public class GreedyAgent extends Player {
 
+	PriorityQueue<State> searchHeap;
+	Game game = Game.getInstance();
+	
 	public GreedyAgent(int id) {
 		super(id);
 	}
@@ -11,7 +20,31 @@ public class GreedyAgent extends Player {
 	@Override
 	public void play() {
 		// TODO Auto-generated method stub
-
+		int bonusArmies = super.calculateBonusArmay();
+		placeBonusArmies(bonusArmies);
+		searchHeap = new PriorityQueue<>();
+		State current = new State();
+		current.setPlayerTerritories(new ArrayList<Integer>(getTerritories()));
+		current.calculateHeuristic();
+		addChildren(current);
 	}
 
+	private void placeBonusArmies(int bonusArmies) {
+		
+	}
+	
+	private void addChildren(State parent) {
+		for (Integer territory : parent.getPlayerTerritories()) {
+			List<Integer> adjacentTerritorries = game.getTerritories()[territory].getAdjacentTerrs();
+			for (Integer adj : adjacentTerritorries) {
+				if (!parent.getPlayerTerritories().contains(adj)) {
+					State child = new State();
+					List<Integer> childTerritories = new ArrayList<>(parent.getPlayerTerritories());
+					childTerritories.add(adj);
+					child.calculateHeuristic();
+					searchHeap.add(child);
+				}
+			}
+		}
+	}
 }
