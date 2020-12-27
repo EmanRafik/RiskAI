3b2cf304b6a441c579484d19750b6c19ed57f743
Michael-Ramy <michaelan2001@gmail.com>
2020-12-27 02:19:39 +0200

    fixed state

--- src/models/State.java
+++ src/models/State.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class State implements Comparable<State>{
 
 	private Territory[] territories;
 	private List<Integer> playerTerritories;
+	private Territory[] initialTerritories;
 	private List<Integer> opponentTerritories;
 	
 	private int cost = 0, heuristic = 0;
@@ -58,6 +60,39 @@ public class State implements Comparable<State>{
 	}
 
 	public void calculateHeuristic() {
+//		for (Integer terr: opponentTerritories) {
+//			for(Integer adj : territories[terr].getAdjacentTerrs()) {
+//				if (territories[adj].getHolderID() == Game.getCurrentPlayerId()) {
+//					heuristic += territories[terr].getTroopsCount();
+//				}
+//			}
+//		}
+		/*heuristic = 0;
+		//add number of undefended territories
+		for (Integer territory : playerTerritories) {
+			for (Integer adj : territories[territory].adjacentTerrs) {
+				if (territories[adj].getHolderID() != Game.getCurrentPlayerId() 
+						&& territories[adj].getTroopsCount() > territories[territory].getTroopsCount() + 1) {
+					heuristic++;
+					break;
+				}
+			}
+		}
+		
+		//subtract number of bonus troops in next step
+		heuristic -= Math.max(3, playerTerritories.size() / 3);
+		
+		heuristic += opponentTerritories.size();
+		
+		//subtract number of territories that the player can attack
+		for (Integer territory : opponentTerritories) {
+			for (Integer adj : territories[territory].adjacentTerrs) {
+				if (territories[adj].getHolderID() == Game.getCurrentPlayerId() 
+						&& territories[adj].getTroopsCount() > territories[territory].getTroopsCount() + 1) {
+					heuristic--;
+				}
+			}
+		}*/
 		int possiblePlayerAttacks = 0;
 		int possibleOpponentAttacks = 0;
 		List<Integer> attacked = new ArrayList<Integer>();
@@ -93,12 +128,19 @@ public class State implements Comparable<State>{
 		}
 		
 		heuristic = 2 * (possibleOpponentAttacks - possiblePlayerAttacks) -10* playerTerritories.size() + opponentTerritories.size();
-		
 	}
 
 	@Override
 	public int compareTo(State s) {
+
 		// TODO Auto-generated method stub
+		/*if ((this.getHeuristic() + this.getCost()) > (s.getHeuristic() + s.getCost()))
+			return 1;
+		if ((this.getHeuristic() + this.getCost()) == (s.getHeuristic() + s.getCost()))
+			return 0;
+		if ((this.getHeuristic() + this.getCost()) < (s.getHeuristic() + s.getCost()))
+			return -1;
+		return 0;*/
 		return (this.getHeuristic() + this.getCost()) - (s.getHeuristic() + s.getCost());
 	}
 	
@@ -109,15 +151,53 @@ public class State implements Comparable<State>{
 		return false;
 	}
 	
-	/*
-	 * 
-	 * for (Integer terr: opponentTerritories) {
-			for(Integer adj : territories[terr].getAdjacentTerrs()) {
-				if (territories[adj].getHolderID() == Game.getCurrentPlayerId()) {
-					heuristic += territories[terr].getTroopsCount();
-				}
+	public void distributeBonusTroops(int bonusArmy) {
+		initialTerritories = Game.getCurrentPlayer().cloneTerritories(territories);
+		ArrayList<Double> borderSecurityRatio = new ArrayList<Double>();
+		ArrayList<Pair> arr = new ArrayList<Pair>();
+		for (Integer curTerr : playerTerritories) {
+			if (territories[curTerr].getTroopsCount() == 0) continue;
+			double curBSR = 0.0;
+			for (Integer oppTerr : territories[curTerr].getAdjacentTerrs()) {
+				if (playerTerritories.contains(oppTerr)) continue;
+				curBSR += territories[oppTerr].getTroopsCount();
 			}
+			curBSR = curBSR / (1.0 * territories[curTerr].getTroopsCount());
+			borderSecurityRatio.add(curBSR);
+			arr.add(new Pair(curBSR, curTerr));
 		}
-	 */
+		
+		Collections.sort(arr, Comparator.comparingDouble(value -> value.x));
+		Collections.reverse(arr);
+		
+		List<Pair> weakestTerr = arr.subList(0, Math.min(2, arr.size()));
+		if (weakestTerr.size() == 0) return;
+		int idx = 0;
+		while (bonusArmy > 0) {
+			territories[weakestTerr.get(idx).y].setTroopsCount(territories[weakestTerr.get(idx).y].getTroopsCount() + 1);
+			bonusArmy--;
+			idx = (idx + 1) % weakestTerr.size();
+		}
+	}
+	
+	public void undoDistribute() {
+		this.territories = initialTerritories;
+	}
+	
+	public void swapPlayerTerritories() {
+		List<Integer> temp = playerTerritories;
+		playerTerritories = opponentTerritories;
+		opponentTerritories = temp;
+	}
+	
+	class Pair { 
+	    double x; 
+	    int y; 
+	
+	    public Pair(double x, int y) { 
+	        this.x = x; 
+	        this.y = y; 
+	    } 
+	} 
 
 }
