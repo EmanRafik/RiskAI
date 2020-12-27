05069072072ffd94d9b9c726a8cc4e1bee6b5b85
Michael-Ramy <michaelan2001@gmail.com>
2020-12-27 02:09:33 +0200

    heuristic + A* + realtime A*

--- src/models/State.java
+++ src/models/State.java
@@ -1,7 +1,6 @@
 package models;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -9,7 +8,6 @@ public class State implements Comparable<State>{
 
 	private Territory[] territories;
 	private List<Integer> playerTerritories;
-	private Territory[] initialTerritories;
 	private List<Integer> opponentTerritories;
 	
 	private int cost = 0, heuristic = 0;
@@ -50,8 +48,8 @@ public class State implements Comparable<State>{
 	}
 
 
-	public void calculateCost(int parentCost) {
-		this.cost = parentCost + 1;
+	public void calculateCost(int parentCost, int troopsLost) {
+		this.cost = parentCost + troopsLost;
 	}
 
 
@@ -60,52 +58,48 @@ public class State implements Comparable<State>{
 	}
 
 	public void calculateHeuristic() {
-//		for (Integer terr: opponentTerritories) {
-//			for(Integer adj : territories[terr].getAdjacentTerrs()) {
-//				if (territories[adj].getHolderID() == Game.getCurrentPlayerId()) {
-//					heuristic += territories[terr].getTroopsCount();
-//				}
-//			}
-//		}
-		heuristic = 0;
-		//add number of undefended territories
-		for (Integer territory : playerTerritories) {
-			for (Integer adj : territories[territory].adjacentTerrs) {
-				if (territories[adj].getHolderID() != Game.getCurrentPlayerId() 
-						&& territories[adj].getTroopsCount() > territories[territory].getTroopsCount() + 1) {
-					heuristic++;
-					break;
+		int possiblePlayerAttacks = 0;
+		int possibleOpponentAttacks = 0;
+		List<Integer> attacked = new ArrayList<Integer>();
+		
+		for (Integer terr: playerTerritories) {
+			for (Integer adj: territories[terr].getAdjacentTerrs()) {
+				if (attacked.contains(adj)) {
+					continue;
+				}
+				if (territories[adj].getHolderID() != territories[terr].getHolderID()) {
+					if (territories[terr].getTroopsCount() > territories[adj].getTroopsCount() + 1) {
+						attacked.add(adj);
+						possiblePlayerAttacks++;
+					}
 				}
 			}
 		}
 		
-		//subtract number of bonus troops in next step
-		heuristic -= Math.max(3, playerTerritories.size() / 3);
+		attacked.clear();
 		
-		heuristic += opponentTerritories.size();
-		
-		//subtract number of territories that the player can attack
-		for (Integer territory : opponentTerritories) {
-			for (Integer adj : territories[territory].adjacentTerrs) {
-				if (territories[adj].getHolderID() == Game.getCurrentPlayerId() 
-						&& territories[adj].getTroopsCount() > territories[territory].getTroopsCount() + 1) {
-					heuristic--;
+		for (Integer terr: opponentTerritories) {
+			for (Integer adj: territories[terr].getAdjacentTerrs()) {
+				if (attacked.contains(adj)) {
+					continue;
+				}
+				if (territories[adj].getHolderID() != territories[terr].getHolderID()) {
+					if (territories[terr].getTroopsCount() > territories[adj].getTroopsCount() + 1) {
+						attacked.add(adj);
+						possibleOpponentAttacks++;
+					}
 				}
 			}
 		}
+		
+		heuristic = 2 * (possibleOpponentAttacks - possiblePlayerAttacks) -10* playerTerritories.size() + opponentTerritories.size();
+		
 	}
 
 	@Override
 	public int compareTo(State s) {
-
 		// TODO Auto-generated method stub
-		if ((this.getHeuristic() + this.getCost()) > (s.getHeuristic() + s.getCost()))
-			return 1;
-		if ((this.getHeuristic() + this.getCost()) == (s.getHeuristic() + s.getCost()))
-			return 0;
-		if ((this.getHeuristic() + this.getCost()) < (s.getHeuristic() + s.getCost()))
-			return -1;
-		return 0;
+		return (this.getHeuristic() + this.getCost()) - (s.getHeuristic() + s.getCost());
 	}
 	
 	public boolean isTerminalState() {
@@ -115,53 +109,15 @@ public class State implements Comparable<State>{
 		return false;
 	}
 	
-	public void distributeBonusTroops(int bonusArmy) {
-		initialTerritories = Game.getCurrentPlayer().cloneTerritories(territories);
-		ArrayList<Double> borderSecurityRatio = new ArrayList<Double>();
-		ArrayList<Pair> arr = new ArrayList<Pair>();
-		for (Integer curTerr : playerTerritories) {
-			if (territories[curTerr].getTroopsCount() == 0) continue;
-			double curBSR = 0.0;
-			for (Integer oppTerr : territories[curTerr].getAdjacentTerrs()) {
-				if (playerTerritories.contains(oppTerr)) continue;
-				curBSR += territories[oppTerr].getTroopsCount();
+	/*
+	 * 
+	 * for (Integer terr: opponentTerritories) {
+			for(Integer adj : territories[terr].getAdjacentTerrs()) {
+				if (territories[adj].getHolderID() == Game.getCurrentPlayerId()) {
+					heuristic += territories[terr].getTroopsCount();
+				}
 			}
-			curBSR = curBSR / (1.0 * territories[curTerr].getTroopsCount());
-			borderSecurityRatio.add(curBSR);
-			arr.add(new Pair(curBSR, curTerr));
 		}
-		
-		Collections.sort(arr, Comparator.comparingDouble(value -> value.x));
-		Collections.reverse(arr);
-		
-		List<Pair> weakestTerr = arr.subList(0, Math.min(2, arr.size()));
-		if (weakestTerr.size() == 0) return;
-		int idx = 0;
-		while (bonusArmy > 0) {
-			territories[weakestTerr.get(idx).y].setTroopsCount(territories[weakestTerr.get(idx).y].getTroopsCount() + 1);
-			bonusArmy--;
-			idx = (idx + 1) % weakestTerr.size();
-		}
-	}
-	
-	public void undoDistribute() {
-		this.territories = initialTerritories;
-	}
-	
-	public void swapPlayerTerritories() {
-		List<Integer> temp = playerTerritories;
-		playerTerritories = opponentTerritories;
-		opponentTerritories = temp;
-	}
-	
-	class Pair { 
-	    double x; 
-	    int y; 
-	
-	    public Pair(double x, int y) { 
-	        this.x = x; 
-	        this.y = y; 
-	    } 
-	} 
+	 */
 
 }
--- src/models/agents/RealtimeAStarAgent.java
+++ src/models/agents/RealtimeAStarAgent.java
@@ -1,8 +1,18 @@
 package models.agents;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import controller.GameConfig;
+import models.Game;
 import models.Player;
+import models.State;
+import models.Territory;
 
 public class RealtimeAStarAgent extends Player {
+	private PriorityQueue<State> searchHeap;
+
 
 	public RealtimeAStarAgent(int id) {
 		super(id);
@@ -17,7 +27,68 @@ public class RealtimeAStarAgent extends Player {
 	@Override
 	public void performAttacks() {
 		// TODO Auto-generated method stub
+		searchHeap = new PriorityQueue<>();
+		State current = new State();
+		current.setTerritories(cloneTerritories(Game.getTerritories()));
+		Player opponent = Game.getPlayers()[1 - super.getPlayerID()];
+		current.setOpponentTerritories(new ArrayList<Integer>(opponent.getTerritories()));
+		current.setPlayerTerritories(new ArrayList<Integer>(getTerritories()));
+
+		addChildren(current);
+		State bestState = current;
+		boolean terminal = false;
+		while (searchHeap.size() > 0) {
+			State nextState = searchHeap.poll();
+			if (bestState.compareTo(nextState) > 0) {
+				bestState = nextState;
+			}
+			if (nextState.isTerminalState()) {
+				this.setTerritories(new ArrayList<Integer>(nextState.getPlayerTerritories()));
+				Game.getPlayers()[1 - super.getPlayerID()]
+						.setTerritories(new ArrayList<Integer>(nextState.getOpponentTerritories()));
+				Game.setTerritories(cloneTerritories(nextState.getTerritories()));
+				terminal = true;
+				break;
+			}
+			addChildren(nextState);
+		}
+		if (!terminal) {
+			this.setTerritories(new ArrayList<Integer>(bestState.getPlayerTerritories()));
+			Game.getPlayers()[1 - super.getPlayerID()]
+					.setTerritories(new ArrayList<Integer>(bestState.getOpponentTerritories()));
+			Game.setTerritories(cloneTerritories(bestState.getTerritories()));
+		}
+		GameConfig.updateTerritories(Game.getTerritories());
 		
+	}
+	
+	private void addChildren(State parent) {
+		Territory[] terr = parent.getTerritories();
+		for (Integer territory : parent.getPlayerTerritories()) {
+			List<Integer> adjacentTerritorries = terr[territory].getAdjacentTerrs();
+			for (Integer adj : adjacentTerritorries) {
+				if (parent.getOpponentTerritories().contains(adj)) {
+					if (terr[territory].getTroopsCount() > terr[adj].getTroopsCount() + 1) {
+						State child = new State();
+						int adjTroops = terr[adj].getTroopsCount();
+						List<Integer> childPlayerTerritories = new ArrayList<>(parent.getPlayerTerritories());
+						childPlayerTerritories.add(adj);
+						child.setPlayerTerritories(childPlayerTerritories);
+						List<Integer> childOpponentTerritories = new ArrayList<>(parent.getOpponentTerritories());
+						super.removeTerritory(childOpponentTerritories, adj);
+						child.setOpponentTerritories(childOpponentTerritories);
+						Territory[] childTerr = cloneTerritories(terr);
+						childTerr[adj].setHolderID(super.getPlayerID());
+						childTerr[adj].setTroopsCount(terr[territory].getTroopsCount() - 1 - adjTroops);
+						childTerr[territory].setTroopsCount(1);
+						child.setTerritories(childTerr);
+						child.calculateHeuristic();
+						child.calculateCost(0, 0);
+						searchHeap.add(child);
+					}
+				}
+			}
+		}
 	}
 
 }
